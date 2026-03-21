09d6179035945a3e189e325936dab037669e4883
suryasantra <santrasurya70@gmail.com>
2026-03-22 00:17:58 +0530

    i had change in a variable

--- SimpleInterest.java
+++ SimpleInterest.java
@@ -2,7 +2,7 @@ class SimpleInterest {
     public static void main(String[] args) {
         double principal = 1500.99;
         double rateOfInterest = 4.75;
-        double time = 1.5;
+        double time = 5.1;
         double calculation = principal *rateOfInterest*time/100;
         System.out.println("Simple Interest Of "+principal+" amount with "+rateOfInterest+" rate of interest for "+time+" year time the result is: "+calculation);
     }
